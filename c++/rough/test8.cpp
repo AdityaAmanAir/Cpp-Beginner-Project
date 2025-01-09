2f419ec70552138aa4ef7bf5ca4846fe60b9ac7a
Aditya Aman <amanaditya1947@proton.me>
2025-01-09 03:19:54 +0000

    new test added

---
+++ c++/rough/test8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x=0,p=0,a;
+	cin>>a;
+	while (x<=a){
+	    x++;
+	    if(x==10){
+	        p+=1;
+	        x=0;
+	    }
+	} cout<<p;
+
+}
