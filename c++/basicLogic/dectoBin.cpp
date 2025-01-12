51dd2a83fdc3d2521534d0e84abd28ad3cd23c10
Aditya Aman <amanaditya1947@proton.me>
2025-01-12 16:04:21 +0000

    update

---
+++ c++/basicLogic/dectoBin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int dtob (int num) {
+    int q,r=0,x=0,t=1;
+    while (num!=0){q=num%2;
+    num/=2;
+    r=(q*(t))+r;
+    t*=10;
+    }
+    return r;
+}
+int main(){
+int n;
+    cout<<"This programm will convert decimal to binary number. Enter the base 10 number : ";
+    cin>>n;
+    cout<<dtob(n);
+    return 0;
+}
