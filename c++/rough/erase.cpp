7838ae59fc090da3d27b78332a92af006bda3110
Aditya Aman <amanaditya1947@proton.me>
2025-01-17 18:38:36 +0000

    daily dose

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,11 +1,7 @@
     #include<bits/stdc++.h>
     using namespace std;
     int main(){
-        int n,k,s=1;
-        cin>>n>>k;
-        while (k--)
-        {
-            s*=n;
-        }
-        cout<<s;
+        double a,b;
+        cin>>a>>b;
+        cout<< setprecision(13)<<((a*b)/(a+b));
     }
