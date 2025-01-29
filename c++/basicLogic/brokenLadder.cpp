920895e5be55146d273291752bf42014e9e408da
AdityaAmanAir <amanaditya1947@proton.me>
2025-01-30 00:03:45 +0530

    update +1

---
+++ c++/basicLogic/brokenLadder.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,k,b=0;
+    bool o=true;
+    cin>>t>>k;
+    while(t--){
+
+        int a;
+        cin>>a;
+        if((a-b)>=k){ 
+            o=false;
+        }else{
+        b=a;
+    }
+   
+    } if(o==true){
+            cout<<"YES";}
+            else{
+cout<<"NO";
+            }
+
+    return 0;
+}
