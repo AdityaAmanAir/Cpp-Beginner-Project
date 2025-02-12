ba21f060a1c68ac2acce1181c3ecc86c87130efb
AdityaAmanAir <amanaditya1947@proton.me>
2025-02-12 23:59:20 +0530

    Update 1202

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 int main (){
-      int a,b,c;
-      cin>>a>>b>>c;
-      cout<<a<<b<<c;
+      int t,w=9999999;
+      cin>>t;
+      int count=0;
+      while(t--){
+         int n, k;
+         cin>>n>>k;
+         while (n--){
+            int m;
+            cin>>m;
+            if (w>m){
+               count++;
+            }
+            w=m;
+         }if(count>k){
+            cout<<"YES\n";
+         }else{cout<<"NO\n";}
+      }
    return 0;
 }
---
+++ py/basicLogic/plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a=np.array([1,2,3,4])
+b=np.array([16,19,23,26])
+
+plt.plot(a,b)
