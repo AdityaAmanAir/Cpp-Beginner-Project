bdc6a8b6d9da82e931aeead2cadff9f8f754aa2b
AdityaAmanAir <amanaditya1947@proton.me>
2025-03-02 00:32:33 +0530

    Update 03.01v

--- c++/rough/erase.cpp
+++ c++/rough/erase.cpp
@@ -1,19 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){ 
-        int a;
-        cin>>a;
-        a-=1;
-        if(a%3==0){
-            cout<<"YES\n";
-        }else{
-            cout<<"NO\n";
-        }
+int min_steps_to_sort_books(int n, vector<int>& p) {
+    vector<int> pos(n + 1);
+    for (int i = 0; i < n; i++) {
+        pos[p[i]] = i;
     }
+
+    int longest_seq = 1;
+    int current_length = 1;
+    for (int i = 2; i <= n; i++) {
+        if (pos[i] > pos[i - 1]) {
+            current_length++;
+        } else {
+            current_length = 1;
+        }
+        longest_seq = max(longest_seq, current_length);
+    }
+
+    return n - longest_seq;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+    cout << min_steps_to_sort_books(n, p) << endl;
     return 0;
 }
-	
--- py/rough/erase.py
+++ py/rough/erase.py
@@ -1,3 +1,9 @@
-def display():
-      print(sum)     
-display()    
+a=int(input())
+while(a>0):
+    a-=1
+    s=str(input())
+    n=len(s)
+    d=""
+    for i in range(0,n-2):
+      d=d+str(s[i])
+    print(d+"i")
