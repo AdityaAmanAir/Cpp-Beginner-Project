28c96f0ed4aeee0f30a1aa49bf86f6bc7329d85e
AdityaAmanAir <amanaditya1947@proton.me>
2025-03-04 00:04:08 +0530

    update 03.03v

--- py/rough/erase.py
+++ py/rough/erase.py
@@ -1,7 +1,28 @@
-s=str(input())
-a=s.count("a")
-b=s.count("b")
-if a+b == len(s):
-  print("Yes")
+a=int(input())
+b=str(input())
+x,y,z=map(int,input().split("+"))
+m=[]
+n=()
+p={}
+m.append("vit")
+print(m.append(7)) #none
+if a>5:
+  pass
+elif a>-100:
+  print("noway")
 else:
-  print("NO")  
+  x="m"
+t=10  
+while t>1:
+  print("lol")
+  break
+else:
+  print("fool")  
+for i in range(1,101,5):
+  print("for")
+#+-* ** /  // 
+and or & ^ |
+<< >>
+abs()
+
+
